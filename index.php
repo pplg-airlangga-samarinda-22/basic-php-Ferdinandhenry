58e0d12e729d8c9094dac16998a08d5feaa1206b
Ferdinandhenry <142764364+Ferdinandhenry@users.noreply.github.com>
2025-01-13 12:01:32 +0700

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+// simpan nama dan nisn klian ke dalam sebuah variabel
+    $nama = "Ferdinand Henry";
+    $nisn = "0084798357";
+// tampilkan atau cetak variabel ke dalam layar 
+    echo 'Nama : ' ,$nama ;
+    echo '<br>';
+    echo 'NISN : ' ,$nisn ;
+?><?php
 
 ?>
